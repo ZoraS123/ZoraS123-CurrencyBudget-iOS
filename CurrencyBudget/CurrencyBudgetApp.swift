32e3f40485a563e5c50b707a40ce1a81dbd81822
zoras12 <zoras@umich.edu>
2026-02-14 14:59:29 -0800

    Initial Commit

---
+++ CurrencyBudget/CurrencyBudgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyBudgetApp.swift
+//  CurrencyBudget
+//
+//  Created by Zora Smith on 2/14/26.
+//
+
+import SwiftUI
+
+@main
+struct CurrencyBudgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
